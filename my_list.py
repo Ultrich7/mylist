4858d4b6339a5126ec8deef9266ee550e9d2f9bc
unknown <you@example.com>
2024-03-05 13:20:28 +0530

    first commit

---
+++ my_list.py
@@ -0,0 +1,15 @@
+my_list=[]
+my_list.append (10)
+my_list.append (20)
+my_list.append (30)
+my_list.append (40)
+my_list.insert (1,15)
+print(my_list)
+my_list.extend([50, 60, 70])
+print(my_list)
+del my_list[-1]
+print(my_list)
+my_list.sort(reverse=True)
+print(my_list)
+index_of_30=my_list.index(30)
+print(index_of_30)
---
+++ user_input.py
@@ -0,0 +1,5 @@
+name= input ('Enter you name:')
+age= input ('Enter you age:')
+location= input ('Enter where you live:')
+print ( 'Hello' ,name,'you are ' ,age ,'years old and you live in ',location,)
+print ('Have a wonderful week' ,name,)
